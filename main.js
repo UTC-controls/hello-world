202931bf64c8b2075041fcfaeac7d5b8b411d616
UTC-controls <78114834+UTC-controls@users.noreply.github.com>
2021-01-28 00:03:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var number = 10;
 		var a = 10, b = 20, c = a + b;
-		
 		//if (number < 20) { alert(c) };
-		document.body.innerHTML = "This is some content ", number;
+		//document.body.innerHTML = "This is some content ", number;
